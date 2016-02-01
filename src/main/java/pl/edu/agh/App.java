ef83fc51b04faa1df80597c44a7d615944947059
liscju <piotr.listkiewicz@gmail.com>
2016-02-01 12:10:01 +0100

    Add setting client properties from command line

--- src/main/java/pl/edu/agh/App.java
+++ src/main/java/pl/edu/agh/App.java
@@ -1,5 +1,6 @@
 package pl.edu.agh;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.io.IOUtils;
 import smile.Network;
 
@@ -8,9 +9,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.net.URISyntaxException;
+import java.util.*;
 
 public class App {
+    private static final Map<String, List<String>> CLIENT_PROPERTIES =
+            new HashMap<String, List<String>>() {{
+                put("ClientAge", Arrays.asList("YoungClient", "MidAgeClient", "OldClient"));
+                put("ClientMoney", Arrays.asList("PoorClient", "MidWealthClient", "RichClient"));
+                put("ClientFamily", Arrays.asList("SingleClient", "FamilyClient"));
+                put("ClientLocation", Arrays.asList("BigCity", "Village"));
+            }};
+
     public static void main( String[] args ) throws NoSuchFieldException, IllegalAccessException, URISyntaxException, IOException {
+        Network network = configureApp();
+        runApp(network);
+    }
+
+    private static Network configureApp() throws IOException {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 
         System.loadLibrary("jsmile");
@@ -20,24 +35,27 @@ public class App {
 
         Network network = new Network();
         network.readString(networkContent);
+        return network;
+    }
 
-        System.out.println("List of all nodes:");
-        for (String arg : network.getAllNodeIds()) {
-            System.out.println(arg);
+    private static void runApp(Network network) {
+        Scanner in = new Scanner(System.in);
+
+        for (String clientProperty : CLIENT_PROPERTIES.keySet()) {
+            System.out.println("Choose property for: " + clientProperty);
+            System.out.println(CLIENT_PROPERTIES.get(clientProperty));
+            System.out.println("Write number to set property for value(starts from 0)");
+            int propertyIndex = in.nextInt();
+            network.setEvidence(clientProperty,
+                    CLIENT_PROPERTIES.get(clientProperty).get(propertyIndex));
         }
-        System.out.println("End of list of all nodes");
-
-        network.setEvidence("ClientAge", "YoungClient");
-        network.setEvidence("ClientMoney", "RichMoney");
-        network.setEvidence("ClientFamily", "SingleClient");
-        network.setEvidence("ClientLocation", "BigCity");
 
         network.updateBeliefs();
 
         for (int i = 0; i < network.getOutcomeCount("BugattiVeyron"); i++) {
             String parentOutcomeId = network.getOutcomeId("BugattiVeyron", i);
             double expectedUtility = network.getNodeValue("BugattiVeyron")[i];
-            System.out.println("");
+            System.out.println(parentOutcomeId + " " + expectedUtility);
         }
     }
 }
